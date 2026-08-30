a21b30dcf2b2b7314a452f8cf3a529b4e5da8627
Christian Sciberras <christian@sciberras.me>
2026-08-31 00:00:11 +0200

    Improve coverage

--- tests/Unit/PhpDoc/Generics/ExtractorTest.php
+++ tests/Unit/PhpDoc/Generics/ExtractorTest.php
@@ -7,6 +7,8 @@ namespace uuf6429\PHPStanPHPDocTypeResolverTests\Unit\PhpDoc\Generics;
 use PHPUnit\Framework\TestCase;
 use uuf6429\PHPStanPHPDocTypeResolver\PhpDoc\Factory;
 use uuf6429\PHPStanPHPDocTypeResolver\PhpDoc\Generics\Extractor;
+use uuf6429\PHPStanPHPDocTypeResolver\PhpDoc\Generics\GenericTypeMap;
+use uuf6429\PHPStanPHPDocTypeResolver\PhpDoc\Types\TemplateTypeNode;
 
 class ExtractorTest extends TestCase
 {
@@ -19,5 +21,34 @@ class ExtractorTest extends TestCase
 		));
 
 		$extractor->extractFromClassName('InvalidClass');
+	}
+
+	public function testExtractImportTypeFromNonIdentifierThrows(): void
+	{
+		$block = Factory::createInstance()->createFromComment(
+			comment: "/**\n * @phpstan-import-type Foo from T\n */",
+			genericsResolver: new GenericTypeMap(
+				templateTypesMap: ['T' => new TemplateTypeNode('T', null)],
+			),
+		);
+
+		$this->expectExceptionObject(new \RuntimeException(
+			'PHPStan type import tag should point to an IdentifierTypeNode, got `' . TemplateTypeNode::class . '` instead',
+		));
+
+		$block->getGenericsResolver();
+	}
+
+	public function testExtractImportTypeFromNonClassLikeThrows(): void
+	{
+		$block = Factory::createInstance()->createFromComment(
+			comment: "/**\n * @phpstan-import-type Foo from int\n */",
+		);
+
+		$this->expectExceptionObject(new \RuntimeException(
+			'PHPStan type can only be imported from a simple class-like structure; symbol `int` could not be found',
+		));
+
+		$block->getGenericsResolver();
 	}
 }
